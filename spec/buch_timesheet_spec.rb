01eb704bbd6781597160b24eb43deeaf4b0ea03c
Jonathan Buch <jbuch@synyx.de>
2019-11-24 21:02:17 +0100

    buch timesheet, add more specs for times

--- spec/buch_timesheet_spec.rb
+++ spec/buch_timesheet_spec.rb
@@ -1,9 +1,20 @@
 require 'parser/buch_timesheet'
 
-RSpec.describe  BuchTimesheet, '#parse' do
+RSpec.describe BuchTimesheet, '#parse' do
   context 'with file' do
     it 'parses the file' do
-      BuchTimesheet.new([]).parse('spec/examples/test.buchungen')
+      r = BuchTimesheet.new([]).parse('spec/examples/test.buchungen')
+      expect(r).to_not be_empty
+    end
+
+    it 'parses comma times correctly' do
+      r = BuchTimesheet.new([]).parse('spec/examples/test.buchungen')
+      expect(r[0][:time]).to eq(1.5)
+    end
+
+    it 'parses colon times correctly' do
+      r = BuchTimesheet.new([]).parse('spec/examples/test.buchungen')
+      expect(r[6][:time]).to eq(1.25)
     end
   end
 end
