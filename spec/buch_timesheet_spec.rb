cefef93bc3dd0207c33f9c7dd1605bfd0ccdc4a5
Jonathan Buch <jbuch@synyx.de>
2019-12-02 17:18:13 +0100

    Add spec for invalid .B files

--- spec/buch_timesheet_spec.rb
+++ spec/buch_timesheet_spec.rb
@@ -2,30 +2,41 @@ require 'parser/buch_timesheet'
 require 'validator'
 
 RSpec.describe BuchTimesheet, '#parse' do
+  subject { BuchTimesheet.new([]).parse('spec/examples/test.B') }
+
+  let(:redmine) do
+    redmine = double("redmine")
+    allow(redmine).to receive(:valid_activity?).and_return(true)
+    redmine
+  end
+
   context 'with file' do
     it 'parses the file' do
-      r = BuchTimesheet.new([]).parse('spec/examples/test.B')
-      expect(r).to_not be_empty
+      expect(subject).to_not be_empty
     end
 
     it 'parses comma times correctly' do
-      r = BuchTimesheet.new([]).parse('spec/examples/test.B')
-      expect(r[0][:time]).to eq(1.5)
+      expect(subject[0][:time]).to eq(1.5)
     end
 
     it 'parses colon times correctly' do
-      r = BuchTimesheet.new([]).parse('spec/examples/test.B')
-      expect(r[6][:time]).to eq(1.25)
+      expect(subject[6][:time]).to eq(1.25)
     end
 
     it 'makes for a valid timesheet' do
-      r = BuchTimesheet.new([]).parse('spec/examples/test.B')
-      redmine = double("redmine")
-      allow(redmine).to receive(:valid_activity?).and_return(true)
-
-      v = r.map {|x| Validator.new.validate(x, redmine) }
+      v = subject.map { |x| Validator.new.validate(x, redmine) }
 
       expect(v).to_not include(false)
+    end
+  end
+
+  context 'invalid' do
+    it 'raises on invalid lines' do
+      expect { BuchTimesheet.new([]).parse('spec/examples/invalid.B') }.to raise_exception(/invalid/)
+    end
+
+    it 'raises on invalid times' do
+      expect { BuchTimesheet.new([]).parse('spec/examples/invalid_time.B') }.to raise_exception(/invalid/)
     end
   end
 end
