581bd0610cca4e663910580326e1aa4072653740
Jonathan Buch <jonathan.buch@gmail.com>
2019-12-10 14:20:45 +0100

    parser, add tests for TimesheetParser forwarding calls

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -15,8 +15,39 @@ describe TimesheetParser do
     expect(p).to_not be_nil
   end
 
+  it 'calls through to the parser for parsing' do
+    expect_any_instance_of(MockParser).to receive(:parse).and_return(true)
+
+    tp = TimesheetParser.new('mock', {})
+    expect(tp.parse).to eq(true)
+  end
+
+  it 'calls through to the parser for archiving' do
+    expect_any_instance_of(MockParser).to receive(:archive).and_return(true)
+
+    tp = TimesheetParser.new('mock', {})
+    expect(tp.archive('mockpath', Date.today)).to eq(true)
+  end
+
   it 'raises error on unknown filetypes' do
     expect { TimesheetParser.new('../CHANGELOG.md', {}) }.to raise_error(/file extension/)
   end
 
+end
+
+class MockParser
+  extend TimesheetParser::Base
+
+  def self.parses?(x)
+    x == 'mock'
+  end
+
+  def initialize(t)
+  end
+
+  def parse(f)
+  end
+
+  def archive(*a)
+  end
 end
