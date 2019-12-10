fa4dbe7e0ecd67b9250e707fb068bd164c4b8e01
Jonathan Buch <jonathan.buch@gmail.com>
2019-12-10 18:07:44 +0100

    cmd, integrate functionality from main.rb into commandline app

--- lib/buchungsstreber.rb
+++ lib/buchungsstreber.rb
@@ -28,82 +28,82 @@ module Buchungsstreber
 
       @entries = @timesheet_parser.parse
     end
+
+    def print_title
+      title = "BUCHUNGSSTREBER v#{VERSION}"
+      puts title.bold
+      puts "~" * title.length
+      puts ""
+    end
+
+    def show_overview
+
+      puts "Buchungsübersicht:".bold
+      validator = Validator.new
+      daily_hours = Hash.new(0)
+      valid = true
+      @entries.each do |entry|
+        redmine = @redmines.get(entry[:redmine])
+        valid &= validator.validate(entry, redmine)
+        daily_hours[entry[:date]] += entry[:time]
+
+        weekday = entry[:date].strftime("%a")
+        print "#{weekday}: "
+        time_s = (entry[:time].to_s + "h").ljust(5)
+        print time_s.bold
+        print " @ "
+        issue_title = Utils.fixed_length(redmine.get_issue(entry[:issue]), 50)
+        print issue_title.blue
+        print ": "
+        text = Utils.fixed_length(entry[:text], 30)
+        puts text
+      end
+      puts ""
+
+      @valid = valid
+
+      unless valid
+        puts "Ungültige Buchungen gefunden – Abbruch!".red.bold
+        return
+      end
+
+      @min_date, @max_date = daily_hours.keys.minmax
+      puts "Zu buchende Stunden (#{@min_date} bis #{@max_date}):".bold
+      daily_hours.each do |date, hours|
+        color = Utils.classify_workhours(hours, config)
+        puts "#{date.strftime("%a")}: #{hours}".colorize(color)
+      end
+
+    end
+
+    def valid?
+      @valid
+    end
+
+    def actualize?
+      puts "Buchungen in Redmine übernehmen? (j/N)"
+      cont = gets.chomp
+      unless cont == "j" || cont == "y"
+        puts "Abbruch"
+        return false
+      end
+      true
+    end
+
+    def save_entries
+
+      @entries.each do |entry|
+        puts "Buche #{entry[:time]}h auf \##{entry[:issue]}: #{entry[:text]}"
+        success = @redmines.get(entry[:redmine]).add_time entry
+        puts success ? "→ OK".green : "→ FEHLER".red.bold
+      end
+
+      puts "Buchungen erfolgreich gespeichert".green.bold
+    end
+
+    def archive
+      archive_path = File.expand_path(@config[:archive_path], __dir__)
+      @timesheet_parser.archive(archive_path, @min_date)
+    end
   end
-end
-
-if File.basename($0) == File.basename(__FILE__)
-VERSION = Buchungsstreber::VERSION
-
-using Rainbow
-
-config = Config.load
-
-timesheet_file = File.expand_path(config[:timesheet_file], __dir__)
-timesheet_parser = TimesheetParser.new(timesheet_file, config[:templates])
-redmines = Redmines.new(config[:redmines])
-
-entries = timesheet_parser.parse
-entries = Aggregator.aggregate(entries)
-
-title = "BUCHUNGSSTREBER v#{VERSION}"
-puts title.bold
-puts "~" * title.length
-puts ""
-
-puts "Buchungsübersicht:".bold
-validator = Validator.new
-daily_hours = Hash.new(0)
-valid = true
-entries.each do |entry|
-  redmine = redmines.get(entry[:redmine])
-  valid &= validator.validate(entry, redmine)
-  daily_hours[entry[:date]] += entry[:time]
-
-  weekday = entry[:date].strftime("%a")
-  print "#{weekday}: "
-  time_s = (entry[:time].to_s + "h").ljust(5)
-  print time_s.bold
-  print " @ "
-  begin
-    issue_title = Utils.fixed_length(redmine.get_issue(entry[:issue]), 50)
-    print issue_title.blue
-  rescue StandardError => e
-    valid = false
-    print Utils.fixed_length("<error: #{e.message}>", 50).red
-  end
-  print ": "
-  text = Utils.fixed_length(entry[:text], 30)
-  puts text
-end
-puts ""
-
-unless valid
-  puts "Ungültige Buchungen gefunden – Abbruch!".red.bold
-  exit
-end
-
-min_date, max_date = daily_hours.keys.minmax
-puts "Zu buchende Stunden (#{min_date} bis #{max_date}):".bold
-daily_hours.each do |date, hours|
-  color = Utils.classify_workhours(hours, config)
-  puts "#{date.strftime("%d.%m. (%a)")}: #{hours}".color(color)
-end
-
-puts "Buchungen in Redmine übernehmen? (j/N)"
-cont = gets.chomp
-unless cont == "j" || cont == "y"
-  puts "Abbruch"
-  exit
-end
-
-entries.each do |entry|
-  puts "Buche #{entry[:time]}h auf \##{entry[:issue]}: #{entry[:text]}"
-  success = redmines.get(entry[:redmine]).add_time entry
-  puts success ? "→ OK".green : "→ FEHLER".red.bold
-end
-
-puts "Buchungen erfolgreich gespeichert".green.bold
-
-archive_path = File.expand_path(config[:archive_path], __dir__)
-timesheet_parser.archive(archive_path, min_date)
 end
