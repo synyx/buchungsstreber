7fae5754cc6607890722e50273aabfef9c05a66e
Jonathan Buch <jonathan.buch@gmail.com>
2019-12-11 21:08:10 +0100

    yaml spec, use example dir for testfiles
    This reduces the test-time from 6 seconds to nothing

--- spec/yaml_timesheet_spec.rb
+++ spec/yaml_timesheet_spec.rb
@@ -30,9 +30,9 @@ describe YamlTimesheet, '#archive' do
 
   it 'archives correctly' do
     # Provide the example file in the fake filesystem
-    config = File.expand_path('..', __dir__)
+    config = File.expand_path('examples', __dir__)
     FakeFS::FileSystem.clone(config)
-    timesheet_path = "#{config}/example.buchungen.yml"
+    timesheet_path = "#{config}/test.yml"
 
     subject.archive(timesheet_path, '/archive', Date.parse('2019-06-18'))
 
