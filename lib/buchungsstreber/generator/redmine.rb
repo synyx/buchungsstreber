b0660f54ae88ba3aee34eaa68ce8491305b600da
Jonathan Buch <jbuch@synyx.de>
2020-01-01 19:15:47 +0100

    generator, add redmine generator

---
+++ lib/buchungsstreber/generator/redmine.rb
@@ -0,0 +1,25 @@
+class Generator::Redmine
+  include Generator::Base
+
+  RE = /\s*(?<project>.*) - (?<tracker>.*) #(?<issue>\d+)(?: \((?<state>[^:]+)\))?: (?<title>.*)/
+
+  def initialize(config)
+    @config = config
+  end
+
+  def generate(date)
+    @config.map do |rm|
+      `cmr "#{rm[:url]}" #{rm[:uid]} "#{rm[:rsskey]}" "#{date}"`.lines.map do |line|
+        p [line, line =~ RE]
+        if line =~ RE
+          {
+            issue: $~[:issue],
+            comment: $~[:project] + ' ' + $~[:title],
+            date: date,
+            redmine: rm[:redmine],
+          }
+        end
+      end
+    end
+  end
+end
