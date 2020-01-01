57cddceded86a79bb9c9afd46d35399cbe18a75a
Jonathan Buch <jbuch@synyx.de>
2020-01-01 12:35:22 +0100

    generators, xchat

--- lib/buchungsstreber/generator/xchat.rb
+++ lib/buchungsstreber/generator/xchat.rb
@@ -6,5 +6,14 @@ class Generator::XChat
   end
 
   def generate(date)
+    `cmx #{date}`.lines.map do |line|
+      if line =~ /(?:issues\/|#)(\d{3,5})/
+        {
+          date: date,
+          issue: $1.to_i,
+          comment: line.chomp,
+        }
+      end
+    end.compact
   end
 end
