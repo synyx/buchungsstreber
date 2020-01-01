7b86a0d73c212370eb92965815ea62ec4b0ee654
Jonathan Buch <jbuch@synyx.de>
2020-01-01 12:35:41 +0100

    generator, gcalc

--- lib/buchungsstreber/generator/gcalc.rb
+++ lib/buchungsstreber/generator/gcalc.rb
@@ -6,5 +6,14 @@ class Generator::GCalc
   end
 
   def generate(date)
+    `gcalcli --tsv agenda "#{date}T06:00-1:00" "#{date}T22:00-1:00"`.lines.map do |line|
+      s = line.split(/\t/)
+      t = (Time.parse("#{s[2]} #{s[3]}") - Time.parse("#{s[0]} #{s[1]}")) / 3600
+      {
+          date: date,
+          time: t,
+          text: s[4].chomp,
+      }
+    end
   end
 end
