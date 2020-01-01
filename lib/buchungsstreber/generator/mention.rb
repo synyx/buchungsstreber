6b2bdf0b165a4ee61e9d1165f99d25b48cb35bc6
Jonathan Buch <jbuch@synyx.de>
2020-01-01 12:35:30 +0100

    generators, mentions

--- lib/buchungsstreber/generator/mention.rb
+++ lib/buchungsstreber/generator/mention.rb
@@ -6,5 +6,17 @@ class Generator::Mention
   end
 
   def generate(date)
+    `cmm #{date}`.lines.map do |line|
+      if line =~ /^\d+/
+        s = line.split(/\t/)
+        {
+            date: date,
+            issue: s[0],
+            time: s[1].to_f,
+            comment: "Mention from #{s[2]} in Project #{s[4]}".chomp,
+            text: s[3].chomp,
+        }
+      end
+    end
   end
 end
