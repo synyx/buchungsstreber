b01df568239c19296aa220017da18b8104bc9005
Jonathan Buch <jbuch@synyx.de>
2020-01-06 18:10:20 +0100

    aggregator, also allow same-text for aggregation

--- lib/aggregator.rb
+++ lib/aggregator.rb
@@ -4,15 +4,11 @@ class Aggregator
 
     aggregated_entries = []
     entries.each do |entry|
-      if entry[:text]
+      possible_aggregations = aggregated_entries.select { |aggregated_entry| aggregatable?(aggregated_entry, entry) }
+      if possible_aggregations.empty?
         aggregated_entries << entry.dup
       else
-        possible_aggregations = aggregated_entries.select { |aggregated_entry| aggregatable?(aggregated_entry, entry) }
-        if possible_aggregations.empty?
-          aggregated_entries << entry.dup
-        else
-          possible_aggregations[-1][:time] += entry[:time]
-        end
+        possible_aggregations[-1][:time] += entry[:time]
       end
     end
 
@@ -25,6 +21,7 @@ class Aggregator
     return entry[:redmine] == extension[:redmine] &&
         entry[:issue] == extension[:issue] &&
         entry[:date] == extension[:date] &&
+        (entry[:text] == extension[:text] || !extension[:text]) &&
         (entry[:activity] == extension[:activity] || !extension[:activity])
   end
 
