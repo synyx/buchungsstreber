1620f7e0a8d9fea57f85af1855eca586dd0d2b74
Jonathan Buch <jbuch@synyx.de>
2020-04-02 09:05:30 +0200

    gcalc generator, fix for new gcalc versions

--- lib/buchungsstreber/generator/gcalc.rb
+++ lib/buchungsstreber/generator/gcalc.rb
@@ -6,14 +6,22 @@ class Generator::GCalc
   end
 
   def generate(date)
-    `gcalcli --tsv agenda "#{date}T06:00-1:00" "#{date}T22:00-1:00"`.lines.map do |line|
-      s = line.split(/\t/)
-      t = (Time.parse("#{s[2]} #{s[3]}") - Time.parse("#{s[0]} #{s[1]}")) / 3600
-      {
+    `gcalcli agenda --tsv "#{date}T06:00-1:00" "#{date}T22:00-1:00"`.lines.map do |line|
+      begin
+        s = line.split(/\t/)
+        t = (Time.parse("#{s[2]} #{s[3]}") - Time.parse("#{s[0]} #{s[1]}")) / 3600
+        {
+            date: date,
+            time: t,
+            text: s[4].chomp,
+        }
+      rescue ArgumentError => e
+        {
           date: date,
-          time: t,
-          text: s[4].chomp,
-      }
+          time: 0.0,
+          text: line.chomp,
+        }
+      end
     end
   end
 end
