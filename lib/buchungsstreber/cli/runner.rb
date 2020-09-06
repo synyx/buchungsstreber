a7044e7ef7a605eea6102275a24605ed897156c2
Jonathan Buch <jbuch@synyx.de>
2020-09-06 14:44:14 +0200

    runner, simplify indentation

--- lib/buchungsstreber/cli/runner.rb
+++ lib/buchungsstreber/cli/runner.rb
@@ -11,34 +11,30 @@ module Buchungsstreber
       end
 
       def execute!
-        exit_code = begin
-                      # Thor accesses these streams directly rather than letting them be injected, so we replace them...
-                      $stderr = @stderr
-                      $stdin = @stdin
-                      $stdout = @stdout
+        # Thor accesses these streams directly rather than letting them be injected, so we replace them...
+        $stderr = @stderr
+        $stdin = @stdin
+        $stdout = @stdout
 
-                      # Run our normal Thor app the way we know and love.
-                      Buchungsstreber::CLI::App.start(@argv)
+        # Run our normal Thor app the way we know and love.
+        Buchungsstreber::CLI::App.start(@argv)
 
-                      # Thor::Base#start does not have a return value, assume success if no exception is raised.
-                      0
-                    rescue StandardError => e
-                      # The ruby interpreter would pipe this to STDERR and exit 1 in the case of an unhandled exception
-                      b = e.backtrace
-                      @stderr.puts("#{b.shift}: #{e.message} (#{e.class})")
-                      @stderr.puts(b.map { |s| "\tfrom #{s}" }.join("\n"))
-                      1
-                    rescue SystemExit => e
-                      e.status
-                    ensure
-                      # ...then we put the streams back.
-                      $stderr = STDERR
-                      $stdin = STDIN
-                      $stdout = STDOUT
-                    end
-
+        # Thor::Base#start does not have a return value, assume success if no exception is raised.
+        @kernel.exit(0)
+      rescue StandardError => e
+        # The ruby interpreter would pipe this to STDERR and exit 1 in the case of an unhandled exception
+        b = e.backtrace
+        @stderr.puts("#{b.shift}: #{e.message} (#{e.class})")
+        @stderr.puts(b.map { |s| "\tfrom #{s}" }.join("\n"))
+        @kernel.exit(1)
+      rescue SystemExit => e
         # Proxy our exit code back to the injected kernel.
-        @kernel.exit(exit_code)
+        @kernel.exit(e.status)
+      ensure
+        # ...then we put the streams back.
+        $stderr = STDERR
+        $stdin = STDIN
+        $stdout = STDOUT
       end
     end
   end
