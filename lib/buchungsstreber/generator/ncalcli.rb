aa196f306438ee58581c80e06290fbdaa0375bee
Daniela Grammlich <grammlich@synyx.de>
2020-09-15 17:32:40 +0200

    [ncalcli] Ignore dates by regular expression

--- lib/buchungsstreber/generator/ncalcli.rb
+++ lib/buchungsstreber/generator/ncalcli.rb
@@ -6,18 +6,24 @@ class Generator::NCalCLI
   end
 
   def generate(date)
+    if @config.empty?
+      ignore = ''
+    elsif (ignore = Regexp.compile(@config[0]['ignore'], Regexp::MULTILINE))
+    end
     `ncalcli "#{date}"`.lines.map do |line|
       s = line.split(/\t/)
       t = s[2].to_f
       summary = s[3].chomp
       summary =~ /#(\d{3,})/
-      issue = $1
-      {
-          date: date,
-          time: t,
-          text: summary,
-          issue: issue,
-      }
+      unless ignore.match(summary)
+        issue = $1
+        {
+            date: date,
+            time: t,
+            text: summary,
+            issue: issue,
+        }
+      end
     rescue StandardError => e
       {
         date: date,
