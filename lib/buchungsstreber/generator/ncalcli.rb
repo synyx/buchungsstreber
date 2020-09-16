8c1667cfd43d70482eb7b4c98c4cccb3ba4f0af2
Daniela Grammlich <grammlich@synyx.de>
2020-09-16 17:48:49 +0200

    [ncalcli] Move code to constructor and cleanup

--- lib/buchungsstreber/generator/ncalcli.rb
+++ lib/buchungsstreber/generator/ncalcli.rb
@@ -3,19 +3,20 @@ class Generator::NCalCLI
 
   def initialize(config)
     @config = config
+    if @config.empty?
+      @ignore = ''
+    else
+      @ignore = Regexp.compile(@config['ignore'])
+    end
   end
 
   def generate(date)
-    if @config.empty?
-      ignore = ''
-    elsif (ignore = Regexp.compile(@config[0]['ignore'], Regexp::MULTILINE))
-    end
     `ncalcli "#{date}"`.lines.map do |line|
       s = line.split(/\t/)
       t = s[2].to_f
       summary = s[3].chomp
       summary =~ /#(\d{3,})/
-      unless ignore.match(summary)
+      unless @ignore.match(summary)
         issue = $1
         {
             date: date,
