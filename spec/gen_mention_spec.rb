8bee865c4174b2a3a70b6e63f301f5cfa2945caf
Jonathan Buch <jbuch@synyx.de>
2020-09-16 18:28:52 +0200

    mention generator, test no returned entries

--- spec/gen_mention_spec.rb
+++ spec/gen_mention_spec.rb
@@ -1,29 +1,38 @@
 require 'buchungsstreber/generator/mention'
 
 RSpec.describe Generator::Mention do
-  context 'generation' do
-    let(:config) { {} }
-    let(:cmm) do
-      <<EOS
+  let(:config) { {} }
+  let(:command_output) do
+    <<EOS
 ~ possible mentions
 31906	1.75	einhorn	Fehlersuche mit Jo	Bäng!
 EOS
-    end
-    let(:result) do
-      {
-        comment: 'Mention from einhorn in project Bäng!',
-        date: 'today',
-        issue: '31906',
-        text: 'Fehlersuche mit Jo',
-        time: 1.75,
-      }
-    end
+  end
+  let(:result) do
+    {
+      comment: 'Mention from einhorn in project Bäng!',
+      date: 'today',
+      issue: '31906',
+      text: 'Fehlersuche mit Jo',
+      time: 1.75,
+    }
+  end
 
+  context 'generation' do
     subject { described_class.new(config) }
-    before { allow(subject).to receive(:`).and_return(cmm) }
+    before { allow(subject).to receive(:`).and_return(command_output) }
 
     it 'executes' do
       expect(subject.generate('today')).to include(result)
+    end
+  end
+
+  context 'no mentions' do
+    subject { described_class.new(config) }
+    before { allow(subject).to receive(:`).and_return("asdf\nasdf\n") }
+
+    it 'executes' do
+      expect(subject.generate('today').compact).to eql([])
     end
   end
 end
