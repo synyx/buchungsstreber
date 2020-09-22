7a5f2a1c2126dbd5796c747cadc59af7f705417e
Daniela Grammlich <grammlich@synyx.de>
2020-09-22 19:04:35 +0200

    [regexp] Add spec

---
+++ spec/res_regexp_spec.rb
@@ -0,0 +1,42 @@
+require 'buchungsstreber/resolver/regexp'
+
+RSpec.describe Resolver::Regexp do
+  let(:config) do
+    [ { 're' => 'bei Tiffany',
+        'entry' => {
+          'activity' => 'Orga',
+          'issue' => 6325,
+          'redmine' => 'S',
+          'text' => 'Frühstück',
+    } } ]
+  end
+
+  let(:entry) do
+    {
+        date: "2020-10-02",
+        time: 2.0,
+        text: 'Frühstück bei Tiffany',
+        issue: nil,
+    }
+  end
+
+  let(:result) do
+    {
+        date: "2020-10-02",
+        time: 2.0,
+        text: 'Frühstück',
+        issue: 6325,
+        redmine: 'S',
+        activity: 'Orga'
+    }
+  end
+
+  context 'resolution' do
+    subject { described_class.new(config) }
+
+    it 'executes' do
+      subject.resolve(entry)
+      expect(entry).to include(result)
+    end
+  end
+end
