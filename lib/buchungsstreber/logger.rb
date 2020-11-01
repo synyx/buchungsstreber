e5f4fa8ff84bf231de08475d735fc33b81eda927
Jonathan Buch <jbuch@synyx.de>
2020-11-01 11:42:40 +0100

    logger, revise logging output
    Note, that currently this is not very thread-safe...

--- lib/buchungsstreber/logger.rb
+++ lib/buchungsstreber/logger.rb
@@ -7,11 +7,17 @@ module Buchungsstreber
         @logfile = File.open('err.out', File::WRONLY | File::APPEND | File::CREAT)
         @logfile.sync = true
         @logger = {}
+        @formatter = proc do |severity, datetime, prog, msg|
+          dt = datetime.strftime('%Y-%m-%d %H:%M:%S.%6N')
+          tid = Thread.current.object_id
+          "%<t>s [%<tid>X] % 5<s>s -- %<p>s: %<msg>s\n" % { t: dt, tid: tid, s: severity, p: prog, msg: msg }
+        end
       end
       if @logger[progname]
         @logger[progname]
       else
         @logger[progname] = Logger.new(@logfile)
+        @logger[progname].formatter = @formatter
         @logger[progname].progname = progname
         @logger[progname].level = Logger::DEBUG
         @logger[progname].info 'started logging'
