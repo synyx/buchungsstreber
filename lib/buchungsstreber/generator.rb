a41128db35ff0fb00c79dd6862e99ff01813ff56
Jonathan Buch <jbuch@synyx.de>
2021-10-14 16:31:33 +0200

    generators, skip misbehaving generator
    * before, one generator throwing exceptions would halt the generation
      process entirely

--- lib/buchungsstreber/generator.rb
+++ lib/buchungsstreber/generator.rb
@@ -10,7 +10,8 @@ class Buchungsstreber::Generator
     generators.each_with_object([]) do |g, memo|
       config = @config[g.name.split(':').last.downcase]
       generator = g.new(config)
-      memo << generator.generate(date)
+      entries = generator.generate(date) rescue []
+      memo << entries
     end.flatten.compact.uniq do |e|
       [e[:issue], e[:date], e[:comment], e[:text], e[:time], e[:activity], e[:redmine]].map(&:to_s).map(&:downcase).join
     end
