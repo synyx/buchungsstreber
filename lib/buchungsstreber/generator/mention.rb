82d1f2e730b0cc86db3e4225135a435328df38ce
Jonathan Buch <jbuch@synyx.de>
2021-10-14 16:32:26 +0200

    generators, update mention generator to not fail on current behaviour

--- lib/buchungsstreber/generator/mention.rb
+++ lib/buchungsstreber/generator/mention.rb
@@ -9,14 +9,32 @@ class Buchungsstreber::Generator::Mention
     `cmm #{date}`.lines.map do |line|
       next unless line =~ /^\d+/
 
-      s = line.split(/\t/)
-      Buchungsstreber::Entry.new(
-        date: date,
-        issue: s[0],
-        time: s[1].to_f,
-        comment: "Mention from #{s[2]} in project #{s[4]}".chomp,
-        text: s[3].chomp,
-      )
+      s = line.split(/\t/, 5)
+      if s.length == 5
+        Buchungsstreber::Entry.new(
+          date: date,
+          issue: s[0],
+          time: s[1].to_f,
+          comment: "Mention from #{s[2]} in project #{s[4]}".chomp(''),
+          text: s[3].chomp(''),
+        )
+      elsif s.length == 3
+        Buchungsstreber::Entry.new(
+          date: date,
+          issue: s[0],
+          time: s[1].to_f,
+          comment: "Mention from #{s[2]}".chomp(''),
+        )
+      else
+        Buchungsstreber::Entry.new(
+          date: date,
+          issue: s[0],
+          time: 0.0,
+          comment: "#{s[1]}".chomp(''),
+        )
+      end
+    rescue
+      nil
     end
   end
 end
