d38456dd3ad5fc1edfe5e2f56fe21f9d2a28ced7
Jonathan Buch <jbuch@synyx.de>
2022-03-06 17:02:21 +0100

    buch format, don't break on empty file

--- lib/buchungsstreber/parser/buch_timesheet.rb
+++ lib/buchungsstreber/parser/buch_timesheet.rb
@@ -24,7 +24,7 @@ class Buchungsstreber::BuchTimesheet
 
     current = nil
     work_hours = nil
-    File.readlines(@file_path).each do |line|
+    self.read_lines.each do |line|
       case line
       when /^([0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9])/
         # beginning of day
